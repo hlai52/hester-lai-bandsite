6d3b1c19ff3f35327c4a0de3cfab2e9f9d7b3f5a
Hester Lai <h.lai52@gmail.com>
2022-10-23 12:47:24 -0700

    Starting shows section JS

--- script/index-page.js
+++ script/index-page.js
@@ -1,19 +1,19 @@
-// ##### Connor Walton
+const comments = [
+  {
+    name: "Connor Walton",
+    time: "02/17/2021",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    time: "01/09/2021",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    time: "01/09/2021",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
 
-// 02/17/2021
-
-// This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.
-
-// ##### Emilie Beach
-
-// 01/09/2021
-
-// I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.
-
-// ##### Miles Acosta
-
-// 12/20/2020
-
-// I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.
-
-// #### Get In Touch
+// const displayComments =
