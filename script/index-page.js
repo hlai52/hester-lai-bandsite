a794ad09412d0d7bbdb9dd37f14bfc70b37d5451
Hester Lai <h.lai52@gmail.com>
2022-10-28 20:43:55 -0700

    Post comments

--- script/index-page.js
+++ script/index-page.js
@@ -1,10 +1,38 @@
-const apiURL =
-  "https://project-1-api.herokuapp.com/comments?api_key=f2b00800-3d32-4544-877c-4a94a8b11418";
+// const apiURL =
+//   "https://project-1-api.herokuapp.com/comments?api_key=f2b00800-3d32-4544-877c-4a94a8b11418";
+
+// axios
+//   .get(apiURL)
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 //create funciton to get comments, make a post then clear the exisiting comments, then call that getting comment function, when posting comment, clears everything out and gets 4 comments and updatesß
 
 const serverUrl = "https://project-1-api.herokuapp.com";
 let api_key = "";
+
+const uniqueID = () => Math.random().toString(36).substring(2, 9);
+
+// Get random photos for avatars
+const getPhotoUrl = () =>
+  `https://loremflickr.com/350/350?random=${Math.floor(Math.random() * 1000)}`;
+
+// format dd/mm/yyyy
+const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+  const yyyy = date.getFullYear();
+  let mm = date.getMonth() + 1;
+  let dd = date.getDate();
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  return dd + "/" + mm + "/" + yyyy;
+};
+
 const refreshApiKey = () => {
   return axios.get(`${serverUrl}/register`).then((response) => {
     api_key = response.data.api_key;
@@ -32,65 +60,44 @@ const getComments = () => {
 };
 
 const postComment = (name, comment) => {
-  const payload = {
-    name: name,
-    comment: comment,
-  };
-  return axios.post(`${serverUrl}/comments?api_key=${api_key}`);
+  return axios.post(
+    `${serverUrl}/comments?api_key=${api_key}`,
+    {
+      name: name,
+      comment: comment,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
 
-axios
-  .get(apiURL)
-  .then((response) => {
-    console.log(response.data);
-  })
-
-  .catch((error) => {
-    console.log(error);
-  });
-
-const uniqueID = () => Math.random().toString(36).substring(2, 9);
-
-// Get random photos for avatars
-const getPhotoUrl = () =>
-  `https://loremflickr.com/350/350?random=${Math.floor(Math.random() * 1000)}`;
-
-// format dd/mm/yyyy
-const formatDate = (dateStr) => {
-  const date = new Date(dateStr);
-  const yyyy = date.getFullYear();
-  let mm = date.getMonth() + 1;
-  let dd = date.getDate();
-  if (dd < 10) dd = "0" + dd;
-  if (mm < 10) mm = "0" + mm;
-  return dd + "/" + mm + "/" + yyyy;
-};
-
-const formEl = document.getElementById("convoform");
-formEl.addEventListener("submit", (event) => {
+const onSubmit = (event) => {
+  // Prevent page refresh
   event.preventDefault();
 
+  const newComment = {
+    id: uniqueID(),
+    picture: getPhotoUrl(),
+    name: event.target.name.value,
+    time: formatDate(new Date()),
+    text: event.target.comment.value,
+  };
+
   // Add new comment to the array
-  displayComment(
-    {
-      id: uniqueID(),
-      picture: getPhotoUrl(),
-      name: event.target.name.value,
-      time: formatDate(new Date()),
-      text: event.target.comment.value,
-    },
-    true
-  );
+  displayComment(newComment, true);
 
   // reset form
   event.target.name.value = "";
   event.target.comment.value = "";
 
   // Send it to the API then refresh comments
-  postComment()
+  postComment(newComment.name, newComment.text)
     .then(() => getComments())
     .catch((error) => alert(error));
-});
+};
 
 // Render an individual comment and add it to reviews__list
 const displayComment = (comment, showOnTop = false) => {
@@ -135,6 +142,9 @@ const displayComment = (comment, showOnTop = false) => {
 };
 
 const render = () => {
+  const formEl = document.getElementById("convoform");
+  formEl.addEventListener("submit", onSubmit);
+
   refreshApiKey().then(() => getComments());
 };
 
